c9f8e3333757e7499e2094ae01014a95bdf49fa4
dindaang <dinda.anggraini27@gmail.com>
2021-02-16 16:36:54 +0700

    coba

--- resources/views/master_soal/master_soal.blade.php
+++ resources/views/master_soal/master_soal.blade.php
@@ -7,7 +7,7 @@
          <h1>Master Soal</h1>
          <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-            <div class="breadcrumb-item"><a href="#">Forms</a></div>
+            <div class="breadcrumb-item"><a href="#">Formds</a></div>
             <div class="breadcrumb-item">Advanced Forms</div>
          </div>
       </div>
